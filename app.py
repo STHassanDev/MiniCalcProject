b8871f628bdf34c40325a7377dc6f2f26febcb7e
STHassanDev <siakahassantraore@gmail.com>
2023-08-17 02:16:23 -0400

    Created the window

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
-# Nothing to see here for now
-print("hello World")
+import tkinter as tk
+
+class Calculator:
+    def __init__(self) -> None:
+        
+        self.wind = tk.Tk()
+
+        self.wind.geometry("600x600")
+
+        self.wind.config(bg='#E0FFFF')
+
+        self.wind.title('Temperature Converter')
+
+        self.wind.mainloop()
+
+Go = Calculator()
